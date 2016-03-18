2febac24af0a7de63928c76803886d154248fcc5
appu24 <aparajitha.k@cspl.com>
2016-03-18 11:45:54 +0530

    Learnt using bootstrap components to design a web page. Added image folder to render images in the web page.

--- js/MyFirstGitProject.js
+++ js/MyFirstGitProject.js
@@ -4,3 +4,8 @@ sayHello = function()
 	var sayHello = "Hello " + name + "!!! <br> Welcome to Training in GIT.";
 	document.getElementById("sayHello").innerHTML = sayHello;
 }
+
+var app = angular.module('myApp', []);
+app.controller('myCtrl', function($scope) {
+    $scope.loremIpsum = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+});
