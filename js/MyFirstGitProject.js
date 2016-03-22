02db64d89695edb144714f85d69eecf0cacf290b
appu24 <aparajitha.k@cspl.com>
2016-03-22 15:37:13 +0530

    Code optimization of JS file

--- js/MyFirstGitProject.js
+++ js/MyFirstGitProject.js
@@ -1,46 +1,8 @@
-sayHello = function()
+var sayHello = function()
 {
 	var name = document.getElementById("name").value;
 	var sayHello = "Hello " + name + "!!! <br> Welcome to Training in GIT.";
 	document.getElementById("sayHello").innerHTML = sayHello;
-}
-
-var hobby = [];
-var hobbyid;
-var gender;
-var textRegExp = /^[a-zA-Z]*$/;
-var emailRegExp = /^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/;
-var phRegExp = /^\d{10}$/;	
-var dateRegExp = /^(([0-9])|([0-2][0-9])|([3][0-1]))\/((3[01]|[12][0-9]|0[1-9]))\/\d{4}$/;
-var errorText = "";
-
-function validateForm()
-{	
-	hobbyid = 1;
-	for(i=0; i<3; i++)
-	{		
-		if(document.getElementsByName('hobby')[i].checked)
-		{
-			hobby[hobbyid] = document.getElementsByName('hobby')[i].value;
-			hobbyid++;
-		}
-	}
-	for(i=0; i<2; i++)
-	{
-		if(document.getElementsByName('e_gender')[i].checked)
-		{
-			gender = document.getElementsByName('e_gender')[i].value;			
-		}		
-	}			
-	errorText = isEmpty(textRegExp,document.getElementById('e_fname').value,document.getElementById('e_fname').previousElementSibling.innerHTML);
-	errorText += isEmpty(textRegExp,document.getElementById('e_lname').value,document.getElementById('e_lname').previousElementSibling.innerHTML);
-	errorText += isEmpty(emailRegExp,document.getElementById('e_email').value,document.getElementById('e_email').previousElementSibling.innerHTML);
-	errorText += isEmpty(phRegExp,document.getElementById('e_ph').value,document.getElementById('e_ph').previousElementSibling.innerHTML);
-	errorText += isEmpty("",document.getElementById('e_department').value,document.getElementById('e_department').previousElementSibling.innerHTML);
-	errorText += isEmpty(dateRegExp,document.getElementById('e_joiningDate').value,document.getElementById('e_joiningDate').previousElementSibling.innerHTML);
-	errorText += isEmpty('',gender,"Gender");
-	errorText += isEmpty('',hobby,"Hobby");
-	document.getElementById('e_errorText').innerHTML = errorText ;	
 }
 
 function clearData(formID)
@@ -51,27 +13,27 @@ function clearData(formID)
 	hobby = [];
 }
 
-function isEmpty(regExp, checkValue, textVal)
+var hobby = [];
+var hobbyid = 1;
+var gender;
+
+function isValid(elem)
 {	
 	var errorText = '';	
-	if(textVal=="Hobby"){
-  		if(checkValue.length == 0)
-  		{
-  			errorText = "Select a value for " + textVal + ". <br/>";	
-  		} 		
-  	}
-  	if(checkValue === undefined)
-	{
-		errorText = "Select a value for " + textVal + ". <br/>";
-	}
+	var checkValue = elem.value;
+	var textVal = elem.getAttribute("data-label");
+	var inputType = elem.type;
+	var regExp;
+		
 	if(checkValue == "")
 	{		
 		errorText = " " + textVal + " should be filled. <br/> ";		
 	}
 	else
 	{
-		if((regExp != ''))
+		if((elem.getAttribute("data-regexp")))
 		{
+			regExp = new RegExp(elem.getAttribute("data-regexp"));
 			if((!regExp.test(checkValue)))
 			{
 				errorText = "Enter a valid " + textVal + ". <br/>";				
@@ -79,6 +41,41 @@ function isEmpty(regExp, checkValue, textVal)
 		}				
 	}
 	return errorText;
+}
+
+function validateEmpForm()
+{	
+	var errorText = '';
+	var elem = document.getElementById('employeeForm').elements;	
+	for(var i = 0; i < elem.length; i++)
+    {
+    	if(elem[i].getAttribute("data-validate") == "true")
+    	{
+    		if((elem[i].type == "checkbox") && (elem[i].checked))
+    		{
+    			hobby[hobbyid] = elem[i];
+            	hobbyid++;            	
+    		} 
+    		else if((elem[i].type == "radio") && (elem[i].checked))
+    		{
+    			gender = elem[i];
+    		}  
+    		else
+    		{
+    			errorText += isValid(elem[i]);
+    		} 	    		
+    		//console.log(elem[i]);
+    	}		
+	}
+	if(!gender)
+	{
+		errorText += "Select an option for Gender<br/>";
+	}
+	if(hobby.length == 0)
+	{
+		errorText += "Select a value for Hobby<br/>";
+	}
+	document.getElementById('e_errorText').innerHTML = errorText ;	
 }
 
 function onlyNumbers(e) {  
