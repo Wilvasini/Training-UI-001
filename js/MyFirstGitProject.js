6a46f463ac03a5f38bb0c41d057d9e0ae5ccd245
appu24 <aparajitha.k@cspl.com>
2016-03-23 19:23:44 +0530

    Modified key press event

--- js/MyFirstGitProject.js
+++ js/MyFirstGitProject.js
@@ -62,22 +62,12 @@ $(document).ready(function(){
 		$("#employeeForm").trigger("reset");
 		$('#e_errorText').text('');
 	})
+	$("#e_ph").keypress(function(){		
+		onlyNumbers(event);	
+	});
 });
 
 function onlyNumbers(e) {  
-    var keynum;  
-    var keychar;  
-    var numcheck;  
-  
-    if(window.event) // IE  
-    {  
-        keynum = e.keyCode;  
-    }  
-    else if(e.which) // Netscape/Firefox/Opera  
-    {  
-        keynum = e.which;  
-    }  
-    keychar = String.fromCharCode(keynum);  
-    numcheck = /\d/;  
-    return numcheck.test(keychar);  
+    numcheck = /\d/;     
+	return numcheck.test(String.fromCharCode(e.keyCode));
 }  
